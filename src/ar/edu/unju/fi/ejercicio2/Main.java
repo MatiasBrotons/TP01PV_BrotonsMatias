f801231d13acab6e73118031b31d5470c355865f
PC <PC@MATIAS>
2024-04-14 20:46:15 -0300

    ejercicio2

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Noruega";
+		int edad = 14;
+		double Alturaedificio = 97.5;
+		double precioproducto = 8.99;
+		String Numerotelefono = "+5485139063";
+		double cos = Math.cos(0.5);
+		
+		
+		System.out.println ("Nombre de un pais: " + pais);
+		System.out.println ("Edad de una persona: " + edad);
+		System.out.println ("Altura de un edificio: " + Alturaedificio + "metros");
+		System.out.println ("Precio de un producto: " + precioproducto + "dolares");
+		System.out.println ("Numero de telefono: " + Numerotelefono);
+		System.out.println ("Coseno de 0.5: " + cos);
+		
+		
+	}
+
+}
